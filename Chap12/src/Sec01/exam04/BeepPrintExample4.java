ee9735fe833d81e5ebeb800c6f891ad47fcc6686
yongYong0225 <118151866+yongYong0225@users.noreply.github.com>
2022-12-18 02:41:00 +0900

    멀티스레드

--- Chap12/src/Sec01/exam04/BeepPrintExample4.java
+++ Chap12/src/Sec01/exam04/BeepPrintExample4.java
@@ -7,7 +7,7 @@ public class BeepPrintExample4 {
 
         for(int i=0; i<5; i++){
             System.out.printf("띵");
-            try { Thread.sleep(500); } catch(Exception e) {}
+            try { Thread.sleep(500); } catch(Exception e) {} //동시시작
         }
     }
 }
