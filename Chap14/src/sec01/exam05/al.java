a32589b858df016e2ca712b5b12658f2ece2e1e9
yongYong0225 <118151866+yongYong0225@users.noreply.github.com>
2023-01-03 00:09:57 +0900

    no message

---
+++ Chap14/src/sec01/exam05/al.java
@@ -0,0 +1,19 @@
+package sec01.exam05;
+
+public class al {
+    public static void main(String[] args) {
+
+
+        int hours = 0;
+        int money = 9000;
+        int pay;
+
+        if(hours>8){
+            int overtime = hours-8;
+            pay = (money*8) + (overtime*m*1.5);
+        } else {
+            pay = money*hours;
+        }
+
+    }
+}
